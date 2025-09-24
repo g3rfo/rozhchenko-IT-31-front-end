83fa97c08e75e25a180e612ebb3dc162377d7bb2
Ivan Rozhchenko <rozhchenkoivan@gmail.com>
2025-09-24 16:34:14 +0300

    add layout component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    LayoutComponent,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('rozhchenko-it-31');
 }
