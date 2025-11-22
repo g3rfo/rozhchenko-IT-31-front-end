83e94ce079497b7c6306e093f3457847e669b9a6
Ivan Rozhchenko <rozhchenkoivan@gmail.com>
2025-11-22 12:44:48 +0200

    add scale-on-hover custom directive

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { DishCard } from '../../shared/models/dish-card';
 import { RouterLink } from "@angular/router";
 import { IngredientsListPipe } from '../../shared/pipes/ingredients-list-pipe';
+import { ScaleOnHover } from '../../shared/directives/scale-on-hover';
 
 @Component({
   selector: 'app-card',
-  imports: [CommonModule, RouterLink, IngredientsListPipe],
+  imports: [
+    CommonModule,
+    RouterLink,
+    IngredientsListPipe,
+    ScaleOnHover
+  ],
   templateUrl: './card.html',
   styleUrl: './card.css'
 })
---
+++ src/app/shared/directives/scale-on-hover.spec.ts
@@ -0,0 +1,8 @@
+import { ScaleOnHover } from './scale-on-hover';
+
+describe('ScaleOnHover', () => {
+  it('should create an instance', () => {
+    const directive = new ScaleOnHover();
+    expect(directive).toBeTruthy();
+  });
+});
